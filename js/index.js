2e96313e5a496926d53007b20c2b70efa6f67f50
ShaneHenchin <shanehenchin@gmail.com>
2014-01-19 20:49:31 +0000

    update

--- js/index.js
+++ js/index.js
@@ -58,7 +58,7 @@ var app = {
 
         scanner.scan( function (result) { 
 
-            alert("We got a barcode\n" + 
+            alert("Dave is a GENIUS\n" + 
             "Result: " + result.text + "\n" + 
             "Format: " + result.format + "\n" + 
             "Cancelled: " + result.cancelled);  
